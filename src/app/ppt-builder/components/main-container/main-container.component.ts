86459df101ca414153d36b3df0fa6ed1c2255579
Fatih Altuntaş <36915172+fatih-altuntas@users.noreply.github.com>
2019-06-14 15:57:43 +0300

    maint container add tool boxs

--- src/app/ppt-builder/components/main-container/main-container.component.ts
+++ src/app/ppt-builder/components/main-container/main-container.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy, Input } from '@angular/core';
 import { PPtBuilderService } from '@app/ppt-builder/service';
 import { PptElementModel, PPtElementEnum } from '@app/ppt-builder/model';
 import { CdkDragDrop } from '@angular/cdk/drag-drop';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'ppt-main-container',
@@ -9,11 +10,13 @@ import { CdkDragDrop } from '@angular/cdk/drag-drop';
   styleUrls: ['./main-container.component.scss']
 })
 export class MainContainer implements OnInit, OnDestroy {
-  constructor(private _pPtBuilderService: PPtBuilderService) {}
+  constructor(private _pPtBuilderService: PPtBuilderService, private modalService: NgbModal) {}
 
+  closeResult: string;
   pptElementList = this._pPtBuilderService.pptElements;
   done: any[] = [];
   activeElement: PptElementModel = undefined;
+  selectTab: number = 1;
 
   drop(event: CdkDragDrop<string[]>) {
     var item = event.item.data;
@@ -27,7 +30,16 @@ export class MainContainer implements OnInit, OnDestroy {
     this._pPtBuilderService.activeElementSubscription.next(item);
   }
 
-  onAddBarChart() {
+  openModal(content: any) {
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then(
+      result => {
+        this.closeResult = `Closed with: ${result}`;
+      },
+      reason => {
+        this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+      }
+    );
+
     var item = this.pptElementList.filter(item => item.type == PPtElementEnum.Chart)[0];
 
     item.x = '35%';
@@ -38,6 +50,21 @@ export class MainContainer implements OnInit, OnDestroy {
     this.done.push(item);
 
     this._pPtBuilderService.activeElementSubscription.next(item);
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+
+  selectTabType(e: any) {
+    debugger;
+    console.log(e);
   }
 
   ngOnInit() {}
