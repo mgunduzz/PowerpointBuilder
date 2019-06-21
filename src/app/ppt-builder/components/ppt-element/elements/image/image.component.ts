5796e9fa360d488d172e331c33093d8d98e89e23
Fatih Altuntaş <36915172+fatih-altuntas@users.noreply.github.com>
2019-06-21 14:19:29 +0300

    image resize

--- src/app/ppt-builder/components/ppt-element/elements/image/image.component.ts
+++ src/app/ppt-builder/components/ppt-element/elements/image/image.component.ts
@@ -1,16 +1,30 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
 import { PptImageElementModel } from '@app/ppt-builder/model';
 import { DomSanitizer } from '@angular/platform-browser';
+
+declare var $: any;
 
 @Component({
   selector: 'ppt-image-element',
   templateUrl: './image.component.html',
   styleUrls: ['./image.component.scss']
 })
-export class ImageComponent implements OnInit {
+export class ImageComponent implements OnInit, AfterViewInit {
   @Input('element') element: PptImageElementModel;
   isImageActive?: boolean = false;
+  dragDropStatus: boolean = true;
   constructor(public _DomSanitizer: DomSanitizer) {}
 
   ngOnInit() {}
+
+  ngAfterViewInit() {
+    $('#box').resizable({ handles: 'all' });
+  }
+
+  dragDropStatusChange() {
+    this.dragDropStatus = false;
+  }
+  dragDropStatusChange1() {
+    this.dragDropStatus = true;
+  }
 }
