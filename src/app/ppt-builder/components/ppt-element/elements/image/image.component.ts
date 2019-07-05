aca3261b24d306f16da211f7ff5d9857c5e41545
mgunduzz <mzlmgndz@gmail.com>
2019-07-05 12:18:35 +0300

    image ratio bugfix ben mazo feat lil Fatih , ilker a.k.a *Lq a.ka gültepeli

--- src/app/ppt-builder/components/ppt-element/elements/image/image.component.ts
+++ src/app/ppt-builder/components/ppt-element/elements/image/image.component.ts
@@ -33,8 +33,12 @@ export class ImageComponent implements OnInit, AfterViewInit {
     $('#img-example') // Make in memory copy of image to avoid css issues
       .attr('src', $(img).attr('src'))
       .on('load', function() {
-        _this.element.format.formatInputs.naturalWidth = this.width; // Note: $(this).width() will not
-        _this.element.format.formatInputs.naturalWidth = this.height; // work for in memory images.
+        _this.element.format.formatInputs.width.value = this.width; // Note: $(this).width() will not
+        _this.element.format.formatInputs.height.value = this.height; // work for in memory images.
+
+        _this.element.onFormatChange.next(_this.element.format.formatInputs.width);
+        _this.element.onFormatChange.next(_this.element.format.formatInputs.height);
+
         $('#img-example').remove();
       });
   }
