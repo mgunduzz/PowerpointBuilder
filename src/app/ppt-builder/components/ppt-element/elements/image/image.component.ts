2755c6df13c46db9be0931ac0705e5a69c5ef9bb
mgunduzz <mzlmgndz@gmail.com>
2019-07-08 14:57:25 +0300

    mazlum ft. lil fatih slide sayfaları arasında geçişlerde posizyon ve bazı componentlerda size problemi var.

--- src/app/ppt-builder/components/ppt-element/elements/image/image.component.ts
+++ src/app/ppt-builder/components/ppt-element/elements/image/image.component.ts
@@ -33,11 +33,19 @@ export class ImageComponent implements OnInit, AfterViewInit {
     $('#img-example') // Make in memory copy of image to avoid css issues
       .attr('src', $(img).attr('src'))
       .on('load', function() {
-        _this.element.format.formatInputs.naturalWidth = this.width; // Note: $(this).width() will not
-        _this.element.format.formatInputs.naturalHeight = this.height; // work for in memory images.
+        if (
+          _this.element.format.formatInputs.naturalWidth.value == 0 &&
+          _this.element.format.formatInputs.naturalHeight.value == 0
+        ) {
+          _this.element.format.formatInputs.naturalWidth = this.width;
+          _this.element.format.formatInputs.naturalHeight = this.height;
 
-        _this.element.format.formatInputs.width.value = this.width; // Note: $(this).width() will not
-        _this.element.format.formatInputs.height.value = this.height; // work for in memory images.
+          _this.element.format.formatInputs.width.value = this.width;
+          _this.element.format.formatInputs.height.value = this.height;
+        } else {
+          _this.element.format.formatInputs.width = _this.element.format.formatInputs.naturalWidth;
+          _this.element.format.formatInputs.height = _this.element.format.formatInputs.naturalHeight;
+        }
 
         _this.element.onFormatChange.next(_this.element.format.formatInputs.width);
         _this.element.onFormatChange.next(_this.element.format.formatInputs.height);
@@ -50,11 +58,11 @@ export class ImageComponent implements OnInit, AfterViewInit {
   //   debugger;
   //     let _this = this;
   //     var img = $('#img-'+_this.element.id)[0];
-  //     $('.image-box').prepend('<img id="theImg" />')  // Make in memory copy of image to avoid css issues
+  //     $('.image-box').prepend('<img id="theImg" />')
   //      .attr("src", $(img).attr("src")).on('load',function() {
   //        debugger;
-  //       _this.element.naturalWidth = this.width;   // Note: $(this).width() will not
-  //       _this.element.naturalHeight = this.height; // work for in memory images.
+  //       _this.element.naturalWidth = this.width;
+  //       _this.element.naturalHeight = this.height;
   //     })
 
   //     $('.theImg').remove()
