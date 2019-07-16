40a44e1f577b9b5275c881cd68f7127ea34cdb11
mgunduzz <mzlmgndz@gmail.com>
2019-07-16 14:29:43 +0300

    aaa

--- src/app/ppt-builder/components/ppt-element/elements/text/text-element.component.ts
+++ src/app/ppt-builder/components/ppt-element/elements/text/text-element.component.ts
@@ -51,90 +51,92 @@ export class TextElement implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
-    this.element.onFormatChange.subscribe((res: any) => {
-      let textInput = res.formatInput as FormatTextInputModel;
-      let dropdown = res.formatInput as FormatDropdownInputModel;
-      let checkbox = res.formatInput as FormatCheckboxInputModel;
-      let numberInput = res.formatInput as FormatNumberInputModel;
-      let colorPickerInput = res.formatInput as FormatColorPickerInputModel;
-      let radioInput = res.formatInput as FormatRadioButtonInputModel;
+    this.element.onFormatChange.subscribe(response => {
+      response.forEach(res => {
+        let textInput = res.formatInput as FormatTextInputModel;
+        let dropdown = res.formatInput as FormatDropdownInputModel;
+        let checkbox = res.formatInput as FormatCheckboxInputModel;
+        let numberInput = res.formatInput as FormatNumberInputModel;
+        let colorPickerInput = res.formatInput as FormatColorPickerInputModel;
+        let radioInput = res.formatInput as FormatRadioButtonInputModel;
 
-      switch (res.formatInput.inputId) {
-        case PPtFormatInputsEnum.color:
-          this.element.color = textInput.value;
-          break;
-        case PPtFormatInputsEnum.font:
-          if (dropdown.selectedItemKey > 0) {
-            let index = dropdown.value.findIndex(o => o.key == dropdown.selectedItemKey);
-            if (index > -1) {
-              this.element.font = dropdown.value[index].value;
+        switch (res.formatInput.inputId) {
+          case PPtFormatInputsEnum.color:
+            this.element.color = textInput.value;
+            break;
+          case PPtFormatInputsEnum.font:
+            if (dropdown.selectedItemKey > 0) {
+              let index = dropdown.value.findIndex(o => o.key == dropdown.selectedItemKey);
+              if (index > -1) {
+                this.element.font = dropdown.value[index].value;
+              }
             }
-          }
-          break;
-        case PPtFormatInputsEnum.backgroundColor:
-          this.element.backgroundColor = colorPickerInput.value;
-          break;
-        case PPtFormatInputsEnum.fontSize:
-          this.element.fontSize = numberInput.value + 'px';
-          break;
-        case PPtFormatInputsEnum.textIndent:
-          this.element.indent = numberInput.value + 'px';
-          break;
-        case PPtFormatInputsEnum.isItalic:
-          if (checkbox.value) {
-            this.element.fontStyle = 'italic';
-          } else {
-            this.element.fontStyle = 'unset';
-          }
-          break;
-        case PPtFormatInputsEnum.strokeColor:
-          if (colorPickerInput.value) {
-            this.element.stroke = '3px solid' + colorPickerInput.value;
-          } else this.element.stroke = '3px solid transparent';
-          break;
-        case PPtFormatInputsEnum.isStroke:
-          if (checkbox.value) {
-            this.element.stroke = '3px solid black';
-          } else {
-            this.element.stroke = '3px solid transparent';
-          }
-          break;
-        case PPtFormatInputsEnum.isBold:
-          if (checkbox.value) {
-            this.element.fontWeigth = 600;
-          } else {
-            this.element.fontWeigth = 100;
-          }
-          break;
-        case PPtFormatInputsEnum.width:
-          if (numberInput.value == 0) {
-            this.element.width = 'auto';
-          } else {
-            this.element.width = numberInput.value + 'px';
-          }
-          break;
-        case PPtFormatInputsEnum.radius:
-          this.element.radius = numberInput.value + 'px';
-          break;
-        case PPtFormatInputsEnum.textAlign:
-          switch (radioInput.selectedItemKey) {
-            case 1:
-              this.element.textAlign = 'left';
-              break;
-            case 2:
-              this.element.textAlign = 'center';
-              break;
-            case 3:
-              this.element.textAlign = 'right';
-              break;
-            default:
-              break;
-          }
+            break;
+          case PPtFormatInputsEnum.backgroundColor:
+            this.element.backgroundColor = colorPickerInput.value;
+            break;
+          case PPtFormatInputsEnum.fontSize:
+            this.element.fontSize = numberInput.value + 'px';
+            break;
+          case PPtFormatInputsEnum.textIndent:
+            this.element.indent = numberInput.value + 'px';
+            break;
+          case PPtFormatInputsEnum.isItalic:
+            if (checkbox.value) {
+              this.element.fontStyle = 'italic';
+            } else {
+              this.element.fontStyle = 'unset';
+            }
+            break;
+          case PPtFormatInputsEnum.strokeColor:
+            if (colorPickerInput.value) {
+              this.element.stroke = '3px solid' + colorPickerInput.value;
+            } else this.element.stroke = '3px solid transparent';
+            break;
+          case PPtFormatInputsEnum.isStroke:
+            if (checkbox.value) {
+              this.element.stroke = '3px solid black';
+            } else {
+              this.element.stroke = '3px solid transparent';
+            }
+            break;
+          case PPtFormatInputsEnum.isBold:
+            if (checkbox.value) {
+              this.element.fontWeigth = 600;
+            } else {
+              this.element.fontWeigth = 100;
+            }
+            break;
+          case PPtFormatInputsEnum.width:
+            if (numberInput.value == 0) {
+              this.element.width = 'auto';
+            } else {
+              this.element.width = numberInput.value + 'px';
+            }
+            break;
+          case PPtFormatInputsEnum.radius:
+            this.element.radius = numberInput.value + 'px';
+            break;
+          case PPtFormatInputsEnum.textAlign:
+            switch (radioInput.selectedItemKey) {
+              case 1:
+                this.element.textAlign = 'left';
+                break;
+              case 2:
+                this.element.textAlign = 'center';
+                break;
+              case 3:
+                this.element.textAlign = 'right';
+                break;
+              default:
+                break;
+            }
 
-          break;
-        default:
-          break;
-      }
+            break;
+          default:
+            break;
+        }
+      });
     });
 
     console.log(this.element);
